73f877f6cef25d6c8cd5f21547723c349736a908
Michael Adams <me@michdavidadams.com>
2022-06-28 16:39:07 -0400

    Stop workout detail view from constantly trying to update exercises

--- Woof WatchKit Extension/Views/Metrics Views/WalkMetricsView.swift
+++ Woof WatchKit Extension/Views/Metrics Views/WalkMetricsView.swift
@@ -10,13 +10,14 @@ import HealthKit
 
 struct WalkMetricsView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
+    @AppStorage("todaysExercise") var todaysExercise: Int = 0
     
     var body: some View {
         
         TimelineView(MetricsTimelineSchedule(from: workoutManager.builder?.startDate ?? Date())) { context in
             VStack(alignment: .leading) {
                 // Displays total workout time; combines today's exercise variable with current workout time
-                TotalTimeProgressView(current: (Date.now.timeIntervalSince(workoutManager.builder?.startDate ?? Date.now) + Double(workoutManager.getTodaysExercise())), selectedWorkout: workoutManager.selectedWorkout)
+                TotalTimeProgressView(current: (Date.now.timeIntervalSince(workoutManager.builder?.startDate ?? Date.now) + Double(todaysExercise)), selectedWorkout: workoutManager.selectedWorkout)
                     .padding()
                     .scenePadding()
                 VStack(alignment: .leading) {
